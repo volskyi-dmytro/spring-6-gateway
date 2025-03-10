3ddea594c245d6bc7147decc08b0d3c114a4c20d
@stpunk47 <stpunk47@gmail.com>
2025-03-10 20:01:40 +0200

    Initial commit

---
+++ src/main/java/com/stpunk47/spring_6_gateway/Spring6GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stpunk47/spring_6_gateway/Spring6GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
